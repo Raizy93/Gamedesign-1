3a0faf712567fcd33a56360b4c20a8083052a782
Raizy93 <dannyklok93@gmail.com>
2026-02-28 23:50:13 +0100

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,10 @@
+// voorbeeldstructuur van 1 artikel — later makkelijk uitbreidbaar
+const artikelen = [
+  {
+    id: 1,
+    titel: "Nieuwe speeltuin geopend",
+    tekst: "De gemeente heeft een nieuwe speeltuin geopend in het centrum...",
+    opties: ["Het park is gesloten", "De speeltuin is vernieuwd", "Er is een museum geopend", "De school is gesloten"],
+    juisteIndex: 1
+  }
+];
